9f0530e6d13e2f7aa22df9da4a7cba5a1c368c60
Greg Hendershott <greghendershott@gmail.com>
2013-09-05 13:54:34 -0400

    Use syntax-class instead of helper function for common code.
    As suggested by @stchang

--- main.rkt
+++ main.rkt
@@ -11,118 +11,96 @@
 (provide defn defn-
          defn/typed defn-/typed)
 
+(begin-for-syntax
+ (define-syntax-class arg
+   #:description "function argument: [ maybe-#:kw id contract maybe-default ]"
+   (pattern [(~seq id:id type:expr)]
+            #:attr decl #'(id)
+            #:with req? #t)
+   (pattern [(~seq kw:keyword id:id type:expr)]
+            #:attr decl #'(kw id)
+            #:with req? #t)
+   (pattern [(~seq id:id type:expr default:expr)]
+            #:attr decl #'([id default])
+            #:with req? #f)
+   (pattern [(~seq kw:keyword id:id type:expr default:expr)]
+            #:attr decl #'(kw [id default])
+            #:with req? #f))
+ (define-syntax-class arrow
+   #:description "-> or =>"
+   (pattern (~or (~datum ->) (~datum =>))))
+ (define-syntax-class def
+   (pattern
+    (defn (ID ARG:arg ... _:arrow RET-TYPE)
+      (~seq #:doc DOC-STR) ...
+      (~seq #:ex [EX-ARGS ... _:arrow EX-RESULT]) ...
+      BODY ...+)
+    #:attr req?s (syntax->datum #'(ARG.req? ...))
+    #:attr types (syntax->list  #'(ARG.type ...))
+    #:with
+    (REQ-ARG-TYPES ...)
+    (for/list ([req? (attribute req?s)] [type (attribute types)] #:when req?)
+      type)
+    #:with
+    (OPT-ARG-TYPES ...)
+    (for/list ([req? (attribute req?s)] [type (attribute types)] #:unless req?)
+      type)
+    #:with
+    CONTRACT #'(->* (REQ-ARG-TYPES ...) (OPT-ARG-TYPES ...) RET-TYPE)
+    #:with
+    COLON #'(: ID (REQ-ARG-TYPES ... -> RET-TYPE)) ;TO-DO: Opt & kw args
+    #:with
+    ((ARG-DECL ...) ...) #'(ARG.decl ...)
+    #:with
+    SIG #'(ID ARG-DECL ... ...)
+    #:with
+    TEST #'(module+ test
+             (require rackunit)
+             (check-equal? (ID EX-ARGS ...) EX-RESULT) ...)
+    #:with
+    DOC #`(module+ doc
+            (defproc (ID ARG ...) RET-TYPE
+              DOC-STR ... "\n"
+              #,(cond [(= 0 (length (syntax->list #'(EX-RESULT ...)))) ""]
+                      [else "Examples:\n"])
+              #,@(map (lambda (args res)
+                        (format "> (~a ~a)\n~a\n"
+                                (syntax->datum #'ID)
+                                (string-join
+                                 (map (compose ~a syntax->datum)
+                                      (syntax->list args)))
+                                (syntax->datum res)))
+                      (syntax->list #'((EX-ARGS ...) ...))
+                      (syntax->list #'(EX-RESULT ...))))))))
+
 (define-syntax (defn stx)
-  (core-defn stx
-             (lambda (stx)
-               (syntax-case stx ()
-                 [(ID (ARG-DECL ...) (BODY ...) CONTRACT COLON TEST DOC)
-                  #'(begin
-                      (define (ID ARG-DECL ...)
-                        BODY ...)
-                      (provide (contract-out [ID CONTRACT]))
-                      TEST
-                      DOC)]))))
+  (syntax-parse stx
+    [d:def #`(begin
+               (provide (contract-out [d.ID d.CONTRACT]))
+               (define d.SIG d.BODY ...)
+               d.TEST
+               d.DOC)]))
 
 (define-syntax (defn- stx)
-  (core-defn stx
-             (lambda (stx)
-               (syntax-case stx ()
-                 [(ID (ARG-DECL ...) (BODY ...) CONTRACT COLON TEST DOC)
-                  #'(begin
-                      (define/contract (ID ARG-DECL ...)
-                        CONTRACT
-                        BODY ...)
-                      TEST
-                      DOC)]))))
+  (syntax-parse stx
+    [d:def #'(begin
+               (define/contract d.SIG d.CONTRACT d.BODY ...)
+               d.TEST
+               d.DOC)]))
 
 (define-syntax (defn/typed stx)
-  (core-defn stx
-             (lambda (stx)
-               (syntax-case stx ()
-                 [(ID (ARG-DECL ...) (BODY ...) CONTRACT COLON TEST DOC)
-                  #'(begin
-                      COLON
-                      (define (ID ARG-DECL ...)
-                        BODY ...)
-                      (provide ID))]))))
+  (syntax-parse stx
+    [d:def #'(begin
+               (provide d.ID)
+               d.COLON
+               (define d.SIG d.BODY ...)
+               d.TEST
+               d.DOC)]))
 
 (define-syntax (defn-/typed stx)
-  (core-defn stx
-             (lambda (stx)
-               (syntax-case stx ()
-                 [(ID (ARG-DECL ...) (BODY ...) CONTRACT COLON TEST DOC)
-                  #'(begin
-                      COLON
-                      (define (ID ARG-DECL ...)
-                        BODY ...))]))))
-
-;; Given stx for what is expected to be our function definition form,
-;; and a function, calls the function with stx that is a list of the
-;; interesting pattern variables we constructed. The function can then
-;; `syntax-case` match to extract the pattern variables again and use
-;; them in a template.  (Roughly speaking I suppose this is using a
-;; syntax object like a dictionary or struct, and syntax-case like
-;; dict-ref or a field access. Or more closely, like using
-;; `(match-define (list ....) x)`. But with the difference that the
-;; extacted things are pattern variables that may be used in a syntax
-;; template.)
-(define-for-syntax (core-defn stx f)
-  (define-syntax-class arg
-    #:description "function argument [ [#:keyword] id contract [default] ]"
-    (pattern [(~seq id:id type:expr)]
-             #:attr decl #'(id)
-             #:with req? #t)
-    (pattern [(~seq kw:keyword id:id type:expr)]
-             #:attr decl #'(kw id)
-             #:with req? #t)
-    (pattern [(~seq id:id type:expr default:expr)]
-             #:attr decl #'([id default])
-             #:with req? #f)
-    (pattern [(~seq kw:keyword id:id type:expr default:expr)]
-             #:attr decl #'(kw [id default])
-             #:with req? #f))
-  (define-syntax-class arrow
-    #:description "-> or =>"
-    (pattern (~or (~datum ->) (~datum =>))))
   (syntax-parse stx
-    [(defn (ID ARG:arg ... _:arrow RET-TYPE)
-       (~seq #:doc DOC-STR) ...
-       (~seq #:ex [EX-ARGS ... _:arrow EX-RESULT]) ...
-       BODY ...+)
-     (define req?s (syntax->datum #'(ARG.req? ...)))
-     (define types (syntax->list  #'(ARG.type ...)))
-     (with-syntax*
-       ([(REQ-ARG-TYPES ...)
-         (for/list ([req? req?s] [type types] #:when req?)
-           type)]
-        [(OPT-ARG-TYPES ...)
-         (for/list ([req? req?s] [type types] #:unless req?)
-           type)]
-        [CONTRACT #'(->* (REQ-ARG-TYPES ...) (OPT-ARG-TYPES ...) RET-TYPE)]
-        [COLON #'(: ID (REQ-ARG-TYPES ... -> RET-TYPE))] ;TO-DO: Opt & kw args
-        [((ARG-DECL ...) ...) #'(ARG.decl ...)]
-        [TEST #'(module+ test
-                  (require rackunit)
-                  (check-equal? (ID EX-ARGS ...) EX-RESULT) ...)]
-        [DOC #`(module+ doc
-                 (defproc (ID ARG ...) RET-TYPE
-                   DOC-STR ... "\n"
-                   #,(cond [(= 0 (length (syntax->list #'(EX-RESULT ...)))) ""]
-                           [else "Examples:\n"])
-                   #,@(map (lambda (args res)
-                             (format "> (~a ~a)\n~a\n"
-                                     (syntax->datum #'ID)
-                                     (string-join
-                                      (map (compose ~a syntax->datum)
-                                           (syntax->list args)))
-                                     (syntax->datum res)))
-                           (syntax->list #'((EX-ARGS ...) ...))
-                           (syntax->list #'(EX-RESULT ...))))) ])
-       (f #'(ID
-             (ARG-DECL ... ...)
-             (BODY ...)
-             CONTRACT
-             COLON
-             TEST
-             DOC
-             )))]))
+    [d:def #'(begin
+               d.COLON
+               (define d.SIG d.BODY ...)
+               d.TEST
+               d.DOC)]))
